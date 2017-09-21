f324f540a1308fa6808cc3623cb8cba858728343
Michael Campos <camposmj89@gmail.com>
2017-09-20 18:23:05 -0700

    Seperate the BookList component to its own file. Helps organize the app better. Tried to do the same with Search by creating a Search.js component but there was a problem. Will attempt to fix later

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import {Route, Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Book from './Book'
+import BookList from './BookList'
 
 class BooksApp extends React.Component {
     state = {
-        /**
-        * TODO: Instead of using this state variable to keep track of which page
-        * we're on, use the URL in the browser's address bar. This will ensure that
-        * users can use the browser's back and forward buttons to navigate between
-        * pages, as well as provide a good URL they can bookmark and share.
-        */
         list: [],
         searchText: "",
         searchList: []
@@ -36,7 +31,7 @@ class BooksApp extends React.Component {
                 });
 
                 this.setState(state);
-                
+
                 // Update searchList
                 this.onUpdateSearchText(this.state.searchText);
             });
@@ -140,56 +135,7 @@ class BooksApp extends React.Component {
             )}/>
 
             <Route path="/" exact render={({history}) => (
-                  <div className="list-books">
-                    <div className="list-books-title">
-                      <h1>MyReads</h1>
-                    </div>
-                    <div className="list-books-content">
-                      <div>
-
-                        {/* Currently Reading */}
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                  {this.state.list.filter(book => book.shelf === "currentlyReading").map(book => (
-                                    <Book key={book.id} handleSelect={this.onHandleSelection} book={book}/>
-                                ))}
-                                </ol>
-                            </div>
-                        </div>
-
-                        {/* Want to Read */}
-                        <div className="bookshelf">
-                          <h2 className="bookshelf-title">Want to Read</h2>
-
-                          <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {this.state.list.filter(book => book.shelf === "wantToRead").map(book => (
-                                    <Book key={book.id} handleSelect={this.onHandleSelection} book={book}/>
-                                ))}
-                            </ol>
-                          </div>
-                        </div>
-
-                        {/* Read */}
-                        <div className="bookshelf">
-                          <h2 className="bookshelf-title">Read</h2>
-                          <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {this.state.list.filter(book => book.shelf === "read").map(book => (
-                                    <Book key={book.id} handleSelect={this.onHandleSelection} book={book}/>
-                                ))}
-                            </ol>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="open-search">
-                        <Link to="/search">Add a book</Link>
-                    </div>
-                  </div>
+                  <BookList list={this.state.list} onHandleSelection={this.onHandleSelection}/>
             )}/>
       </div>
     )
